5cd99539b8032323863beede9810c7a81e733e9c
Txapela <123185025+Txapela@users.noreply.github.com>
2024-03-19 19:42:35 +0100

    Update Dia_del_padre.ino

--- Dia_del_padre.ino
+++ Dia_del_padre.ino
@@ -3,8 +3,8 @@ int clockPin = 13; // Pin connected to SH_CP of 74HC595（Pin11）
 int dataPin = 11; // Pin connected to DS of 74HC595（Pin14）
 int LEDPin[] = {2, 3, 4, 5, 6, 7, 8, 9}; // column pin (cathode) of LED Matrix
 
-// Define the pattern data for a smiling face
-const int smilingFace[] = {
+// Define the pattern data for a alien face
+const int alienFace[] = {
  0x00, 0x10, 0xD3, 0xFC, 0xFC, 0xD3, 0x10, 0x00
 };
 
@@ -47,12 +47,12 @@ void setup() {
 void loop() {
  // Define a one-byte variable (8 bits) which is used to represent the selected state of 8 columns.
  int cols;
- // Display the static smiling pattern
+ // Display the static alien pattern
  for (int j = 0; j < 200; j++ ) { // repeat 200 times
  cols = 0x01; // Assign 0x01(binary 00000001) to the variable, which represents the first column is selected.
  for (int i = 0; i < 8; i++) { // display 8 column data by scanning
  matrixColsVal(cols); // select this column
- matrixRowsVal(smilingFace[i]);// display the data in this  column
+ matrixRowsVal(alienFace[i]);// display the data in this  column
  delay(1); // display them for a period of time
  matrixRowsVal(0x00); // clear the data of this column
  cols <<= 1; // shift "cols" 1 bit left to select the next column
